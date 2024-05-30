9d76823096eb73d5c1cb762abfe6720504455d85
unknown <bart.karolczak@gmail.com>
2024-05-30 16:06:05 +0200

    Dodałem coś do BallContoller i folder na Sprites

--- Bowling Simulator/Assets/Scripts/BallController.cs
+++ Bowling Simulator/Assets/Scripts/BallController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float horizontalSpeed = 5f;
+    private float verticalSpeed = 100f;
+    private float horizontal;
+    private bool isMoving = false;
+
+    [SerializeField] private Rigidbody2D rb;
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontal = Input.GetAxisRaw("Horizontal");
+        if (Input.GetKey(KeyCode.Space))
+        {
+            isMoving = true;
+            rb.velocity = new Vector2(0, 0);
+            rb.AddForce(new Vector2(0, verticalSpeed));
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(!isMoving)
+        {
+            rb.velocity = new Vector2(horizontal * horizontalSpeed, rb.velocity.y);
+        }
     }
 }
