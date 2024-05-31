5ebe37bafd93253b7889a0a7d23e8b13d62e1b03
unknown <bart.karolczak@gmail.com>
2024-06-01 01:13:47 +0200

    Dodałem szczekanie psów w 5 rundzie

--- Bowling Simulator/Assets/Scripts/MiniGame/BowlingMusicController.cs
+++ Bowling Simulator/Assets/Scripts/MiniGame/BowlingMusicController.cs
@@ -8,6 +8,7 @@ public class BowlingMusicController : MonoBehaviour
 
     [SerializeField] private AudioSource source;
     [SerializeField] private AudioClip screamSound;
+    [SerializeField] private AudioClip[] barkSounds;
 
     // Update is called once per frame
     void Update()
@@ -16,7 +17,26 @@ public class BowlingMusicController : MonoBehaviour
         if(currentStage >= StoryStages.Round2 && lastStoryStage != currentStage)
         {
             source.PlayOneShot(screamSound);
-            lastStoryStage = currentStage;
+        }
+        if(currentStage >= StoryStages.Round5 && lastStoryStage != currentStage)
+        {
+            StartCoroutine(PlayRandomBarking());
+        }
+        lastStoryStage = currentStage;
+    }
+
+    private IEnumerator PlayRandomBarking()
+    {
+        while (true)
+        {
+            int randomIndex = Random.Range(0, barkSounds.Length);
+            AudioClip randomBark = barkSounds[randomIndex];
+            
+            source.PlayOneShot(randomBark);
+
+            yield return new WaitForSeconds(randomBark.length);
+            float randomDelay = Random.Range(1, 5);
+            yield return new WaitForSeconds(randomDelay);
         }
     }
 }
