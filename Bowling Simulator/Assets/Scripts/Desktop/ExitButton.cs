23737b02786bc0412310595389e5be65c917a0d5
nitrazek <99548252+nitrazek@users.noreply.github.com>
2024-06-01 12:18:46 +0200

    Drobne poprawki

--- Bowling Simulator/Assets/Scripts/Desktop/ExitButton.cs
+++ Bowling Simulator/Assets/Scripts/Desktop/ExitButton.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ExitButton : MonoBehaviour
 {
+    public RawImage sceneFrame;
     public void ExitGame()
     {
+        if (sceneFrame.gameObject.activeSelf)
+            return;
+
         Application.Quit();
     }
 }
